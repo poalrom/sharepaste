ff47e812e21f6c654c8bc775b22c402bdf824f1c
Alex Popkov <poalrom@yandex.ru>
2026-05-02 13:23:27 +0200

    chore: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import 'dotenv/config';
 import { Command } from "commander";
 import { runUserCreate, runUserList, runUserDelete } from "./cli/user.js";
 import { runDeviceList, runDeviceRevoke } from "./cli/device.js";
